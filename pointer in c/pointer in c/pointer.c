4a5e4abf66a6bcabbcd624c0ceef4018f6c22341
selin pir <113732977+selinpir@users.noreply.github.com>
2024-01-18 16:54:35 +0300

    .

---
+++ pointer in c/pointer in c/pointer.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+	char dizi[100];
+	char eleme;
+	char* selin;
+
+	printf("Karakter dizisi giriniz:");
+	gets(dizi);
+	printf("karakter dizisinden cikarilacak karakter");
+
+	return 0;
+
+}
