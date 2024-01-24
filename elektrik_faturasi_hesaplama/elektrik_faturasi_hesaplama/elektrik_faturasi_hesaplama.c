fdff90038cb7879b39ab6f16702e9a989b902d51
selin pir <113732977+selinpir@users.noreply.github.com>
2024-01-24 13:43:41 +0300

    .

---
+++ elektrik_faturasi_hesaplama/elektrik_faturasi_hesaplama/elektrik_faturasi_hesaplama.c
@@ -0,0 +1,34 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+
+	printf("Elektrik Faturasi Hesaplama Uygulamasi\n");
+
+	float odenecekFatura, eskiBirimFiyat, yeniBirimFiyat;
+	float enflasyon, eskiAboneUcreti, yeniAboneUcreti;
+	int yeniOkuma, eskiOkuma, toplamHarcananElektrik;
+
+	printf("Enflasyon oranini giriniz:");
+	scanf_s("%f", &enflasyon);
+
+	printf("Onceki aya ait birim miktari giriniz:");
+	scanf_s("%f", &eskiBirimFiyat);
+
+	printf("Onceki aya ait abonelik ucretini giriniz:");
+	scanf_s("%f", &eskiAboneUcreti);
+
+	printf("Bir onceki aya ait okuma degerini giriniz:");
+	scanf_s("%d", &eskiOkuma);
+
+	printf("Bu aya ait okuma degerini giriniz:");
+	scanf_s("%d", &yeniOkuma);
+
+	toplamHarcananElektrik = yeniOkuma - eskiOkuma; //harcanan elektrigi buluruz
+	yeniBirimFiyat = eskiBirimFiyat*(1+enflasyon/100);
+	yeniAboneUcreti = eskiAboneUcreti * (1 + enflasyon / 100);
+
+	odenecekFatura = toplamHarcananElektrik * yeniBirimFiyat + yeniAboneUcreti;
+
+	printf("\nOdeyeceginiz fatura tutari: %f: \n\n", odenecekFatura);
+}
