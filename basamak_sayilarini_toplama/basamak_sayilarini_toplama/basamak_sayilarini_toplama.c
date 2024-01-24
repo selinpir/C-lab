3e5da90b72d038bfddb15183e5b8c84fb740a497
selin pir <113732977+selinpir@users.noreply.github.com>
2024-01-24 13:16:30 +0300

    .

---
+++ basamak_sayilarini_toplama/basamak_sayilarini_toplama/basamak_sayilarini_toplama.c
@@ -0,0 +1,30 @@
+﻿#include<stdio.h>
+int main()
+{
+
+	printf("Girilen Sayinin Rakamlarini Toplayan Uygulama\n");
+	int sayim, bolum, kalan, toplam;
+	toplam = 0;
+
+	printf("4 basamakli sayi giriniz\n");
+	scanf_s("%d", &sayim);
+	 //4. basamak bulunur
+	bolum = sayim / 1000;
+	toplam += bolum;
+	kalan = sayim % 1000;
+
+	//3. basamak bulunur
+	bolum = kalan / 100;
+	toplam += bolum;
+	kalan = kalan % 100;
+
+	//2. basamak bulunur
+	bolum = kalan / 10;
+	toplam += bolum;
+	kalan = kalan % 10;
+
+	//1. basamak bulunur
+	toplam += kalan;
+	printf("Girdiginiz sayinin rakamlar toplami : %d\n",toplam);
+
+}
