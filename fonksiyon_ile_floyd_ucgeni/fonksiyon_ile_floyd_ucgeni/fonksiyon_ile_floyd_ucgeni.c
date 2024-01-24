e9fbd4e877e74f3dd162123c35ca9bfd91ea279c
selin pir <113732977+selinpir@users.noreply.github.com>
2024-01-24 17:13:38 +0300

    .

---
+++ fonksiyon_ile_floyd_ucgeni/fonksiyon_ile_floyd_ucgeni/fonksiyon_ile_floyd_ucgeni.c
@@ -0,0 +1,31 @@
+﻿#include<stdio.h>
+
+void floyd(int n)
+{
+	int i = 1;
+	int j ;
+	int a=1;
+
+	while (i <= n)
+	{
+		j = 1;
+		while (j <= i)
+		{
+			printf("%4d", a);
+			a++;
+			j++;
+		}
+
+		printf("\n");
+		i++;
+	}
+}
+
+int main()
+{
+	int satir;
+	printf("floyd ucgeni icin satir sayisini giriniz: ");
+	scanf_s("%d", &satir);
+	floyd(satir);
+
+}
