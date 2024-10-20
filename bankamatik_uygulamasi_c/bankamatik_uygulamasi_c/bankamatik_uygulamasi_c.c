9335f43a2086a81065883ecdb3b94bd394f1c199
selin pir <113732977+selinpir@users.noreply.github.com>
2024-10-20 22:17:35 +0300

    2010

---
+++ bankamatik_uygulamasi_c/bankamatik_uygulamasi_c/bankamatik_uygulamasi_c.c
@@ -0,0 +1,70 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int no, bakiye, secim;
+	char ad[30];
+	FILE* dosya;
+
+	// Dosyayı okuma modunda açıyoruz
+	if ((dosya = fopen("musteriler.txt", "r")) == NULL) // "r" okuma modu
+	{
+		printf("Dosya bulunamadi.\n");
+		return 1; // Eğer dosya açılamazsa programdan çık
+	}
+
+	while (1)
+	{
+		printf("\nBanka Bilgileri Uygulamasi\n");
+		printf("1-Hesabinda para olan musteriler\n");
+		printf("2-Hesabinda para olmayan musteriler\n");
+		printf("3-Borcu olan musteriler\n");
+		printf("4-Cikis\n");
+
+		printf("Seciminizi girin: ");
+		scanf("%d", &secim);
+
+		switch (secim)
+		{
+		case 1:
+			printf("\nHesabinda para olan musteriler:\n");
+			rewind(dosya); // Dosya başına dön
+			while (fscanf(dosya, "%d\t%s\t%d", &no, ad, &bakiye) == 3)
+			{
+				if (bakiye > 0)
+					printf("%d\t%s\t%d\n", no, ad, bakiye);
+			}
+			break;
+
+		case 2:
+			printf("\nHesabinda para olmayan musteriler:\n");
+			rewind(dosya); // Dosya başına dön
+			while (fscanf(dosya, "%d\t%s\t%d", &no, ad, &bakiye) == 3)
+			{
+				if (bakiye == 0)
+					printf("%d\t%s\t%d\n", no, ad, bakiye);
+			}
+			break;
+
+		case 3:
+			printf("\nBorcu olan musteriler:\n");
+			rewind(dosya); // Dosya başına dön
+			while (fscanf(dosya, "%d\t%s\t%d", &no, ad, &bakiye) == 3)
+			{
+				if (bakiye < 0)
+					printf("%d\t%s\t%d\n", no, ad, bakiye);
+			}
+			break;
+
+		case 4:
+			printf("Cikis Yaptiniz ... \n");
+			fclose(dosya); // Dosyayı kapat
+			return 0;
+
+		default:
+			printf("Gecersiz secim! Lutfen tekrar deneyin.\n");
+		}
+	}
+	return 0;
+}
---
+++ ogrenci_kayit_c/ogrenci_kayit_c/ogrenci_kayit_c.c
@@ -0,0 +1,49 @@
+﻿#include<stdio.h>
+
+int main()
+{
+	int no, ort;
+	char ad[30];
+	FILE* dosya;
+
+	// Dosya yazma modu
+	if ((dosya = fopen("ogrKayit.txt", "w")) == NULL)
+	{
+		printf("Dosya Olusturulamadi.\n");
+		return 1;
+	}
+	else
+	{
+		printf("Ogrencinin no, ad ve ortalamasini giriniz:\n");
+		printf("Cikis icin EOF (CTRL+Z) basiniz.\n");
+
+		// İlk veri girişini yapıyoruz
+		while (scanf("%d %s %d", &no, ad, &ort) == 3)  // scanf'in başarılı okuma sayısını kontrol ediyoruz
+		{
+			fprintf(dosya, "%d\t%s\t%d\n", no, ad, ort);
+			printf("\nDevam etmek icin yeni veri giriniz (CTRL+Z ile cikis yapabilirsiniz): ");
+		}
+		fclose(dosya);
+		printf("\nYazma islemi tamamlanmistir, okumaya geciliyor.\n");
+	}
+
+	// Dosya okuma modu
+	if ((dosya = fopen("ogrKayit.txt", "r")) == NULL)
+	{
+		printf("Dosya Bulunamadi.\n");
+		return 1;
+	}
+	else
+	{
+		printf("\nno\tad\tort\n");
+		// fscanf'in dönüş değerini kontrol ederek okuma yapıyoruz
+		while (fscanf(dosya, "%d\t%s\t%d", &no, ad, &ort) == 3) // Eğer 3 öğe (numara, ad ve ortalama) doğru bir şekilde okunursa, döngü devam eder.
+		{
+			printf("%d\t%s\t%d\n", no, ad, ort);
+		}
+		fclose(dosya);
+		printf("\nOkuma islemi tamamlandi.\n");
+	}
+
+	return 0;
+}
